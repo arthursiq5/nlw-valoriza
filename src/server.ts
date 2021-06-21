822c62279ca61f3872b3b735bb96f404e6c80b9e
Arthur Siqueira <arthur@oneweb.com.br>
2021-06-21 00:25:10 -0300

    Criada primeira rota em Node

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,9 @@
-import express from 'express';
+import express, { request, response } from 'express';
 
 const app = express();
+
+app.get('/test', (request, response) => {
+    response.send('Hello World');
+});
+
 app.listen(3000, () => console.log('Server is running'));
