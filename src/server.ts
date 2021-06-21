1dfdbb9d38f29eff1099259d125ccd5e07bc810e
Arthur Siqueira <arthur@oneweb.com.br>
2021-06-21 00:25:40 -0300

    corrigida rota

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express, { request, response } from 'express';
 const app = express();
 
 app.get('/test', (request, response) => {
-    response.send('Hello World');
+    return response.send('Hello World');
 });
 
 app.listen(3000, () => console.log('Server is running'));
