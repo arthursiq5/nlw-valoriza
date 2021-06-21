513bb0bebb14b89f7096c8eca21132394cbdc3cf
Arthur Siqueira <arthur@oneweb.com.br>
2021-06-21 00:32:53 -0300

    Adicionado exemplo de rota POST

--- src/server.ts
+++ src/server.ts
@@ -6,4 +6,8 @@ app.get('/test', (request, response) => {
     return response.send('Hello World');
 });
 
+app.post('test-post', (request, response) => {
+    return response.send('Hello Post Route');
+});
+
 app.listen(3000, () => console.log('Server is running'));
